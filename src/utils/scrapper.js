e28ccca91d7607207af3ae7ab3a2d6c393eb1cbb
Andres Muniz Piniella <amp-git@c4ad.eu>
2025-04-08 11:25:42 +0100

    Found invoice tab

--- src/utils/scrapper.js
+++ src/utils/scrapper.js
@@ -6,66 +6,6 @@ let listOfAccounts = []
 let accountCount = 0
 let listOfPDFs = []
 
-const Scrapper = async (url) => {
-  try {
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: ['--start-maximized'],
-      defaultViewport: null
-    })
-    const page = await browser.newPage()
-    await page.goto(url)
-    console.log('Page open!')
-    logInEDF(page, browser)
-    getAccounts(page, browser, url)
-
-    // await browser.close()
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-const getAccounts = async (page, browser, url) => {
-  await page.locator('tr').wait()
-  console.log('List of accounts page loaded', url)
-  let currentAccounts = await page.$$eval('td > p.sc-eauhAA.efjsHa', (texts) =>
-    texts.map((e) => e.textContent)
-  )
-  currentAccounts = currentAccounts.filter((item) => item !== 'ACTIVE')
-  listOfAccounts = listOfAccounts.concat(currentAccounts)
-  try {
-    const enabled = await page
-      .locator('aria/Next page[role="button"]')
-      .map((button) => !button.disabled)
-      .wait()
-    console.log('Next page enabled?', enabled)
-    if (enabled) {
-      await page.locator('aria/Next page[role="button"]').click()
-      getAccounts(page, browser, url)
-    } else {
-      for (const account of listOfAccounts) {
-        // const accountPage = await browser.newPage()
-        await page.goto(`${url}accounts/${account}/bills-and-payments`)
-        await page.locator('button.sc-ciFVpn.bdCjIk').wait()
-        // click invoices button
-        await page.locator('button.sc-ciFVpn.bdCjIk').click()
-        //get the first document that can be downloaded form the list.
-        const linkToPDF = await page
-          .locator('a.sc-fAUdSK.iZGISN')
-          .map((el) => el.href)
-          .wait()
-        listOfPDFs.push(linkToPDF)
-      }
-    }
-    console.log(listOfPDFs)
-  } catch (error) {
-    console.log(
-      'finished all pages and got an error for not finding the next one',
-      error
-    )
-  }
-  fetchPDFs(listOfPDFs)
-}
 const fetchPDFs = async () => {
   // listOfPDFs.forEach((url, index) => {
   console.log(
@@ -78,7 +18,7 @@ const fetchPDFs = async () => {
     fs.writeFileSync(`${listOfAccounts[index]}.pdf`, binaryPdf, 'binary')
   }
   // for (const url of listOfPDFs) {
-
+  console.log('PDFs downloaded')
   // }
 }
 const logInEDF = async (page, browser) => {
@@ -114,4 +54,78 @@ const ignoreCookies = async (page) => {
     return false
   }
 }
+
+const getAccounts = async (page, browser, url) => {
+  await page.locator('tr').wait()
+  console.log('List of accounts page loaded', url)
+  let currentAccounts = await page.$$eval('td > p.sc-eauhAA.efjsHa', (texts) =>
+    texts.map((e) => e.textContent)
+  )
+  currentAccounts = currentAccounts.filter((item) => item !== 'ACTIVE')
+  listOfAccounts = listOfAccounts.concat(currentAccounts)
+  console.log('List of accounts', listOfAccounts)
+  try {
+    const enabled = await page
+      .locator('aria/Next page[role="button"]')
+      .map((button) => !button.disabled)
+      .wait()
+    console.log('Next page enabled?', enabled)
+    if (enabled) {
+      await page.locator('aria/Next page[role="button"]').click()
+      getAccounts(page, browser, url)
+    } else {
+      for (const account of listOfAccounts) {
+        // const accountPage = await browser.newPage()
+        await page.goto(`${url}accounts/${account}/bills-and-payments`)
+        console.log('Account page loaded', account)
+        try {
+          const invoiceButton = await page
+            .locator('button.sc-dLoHQq.HfQvL[role="tab"]')
+            .waitHandle()
+          await invoiceButton.click()
+
+          const linkToPDF = await page
+            .locator('a.sc-fAUdSK.iZGISN[aria-label*="View"]')
+            .map((el) => el.href)
+            .wait()
+          listOfPDFs.push(linkToPDF)
+        } catch (error) {
+          console.log('Error getting the invoices', error)
+        }
+      }
+    }
+  } catch (error) {
+    console.log(
+      'finished all pages and got an error for not finding the next one',
+      error
+    )
+  }
+  console.log('List of pdfs', listOfPDFs)
+
+  await fetchPDFs(listOfPDFs)
+}
+
+const Scrapper = async (url) => {
+  let browser
+  try {
+    browser = await puppeteer.launch({
+      headless: true //,
+      // args: ['--start-maximized'],
+      // defaultViewport: null
+    })
+    const page = await browser.newPage()
+    await page.goto(url)
+    console.log('Page open!')
+    await logInEDF(page, browser)
+    await getAccounts(page, browser, url)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    if (browser) {
+      // await browser.close()
+      console.log('Browser closed')
+    }
+  }
+}
+
 module.exports = { Scrapper }
